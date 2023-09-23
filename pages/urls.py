ffb06600bc2bc353d57719be4a27814c2584cb2c
kprasannamahesh <kprasannamahesh007@gmail.com>
2023-09-23 20:15:21 +0530

    Added model in the App

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView,contactPageView
 
 urlpatterns = [
-path('', homePageView, name='home'),
+    path('home', homePageView, name='home'),
+    path('contact', contactPageView, name='contact'),    
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+
+def homePageView(request,*args,**kwargs):
+    return render(request,"home.html",{})
+
+
+
+def contactPageView(*args,**kwargs):
+    return HttpResponse('<h1>Contact Us At</h1>')
